e8cb5bd1cde49f23ea60900fe47afe0c16b40583
Nabil Galeb <nabilgaleb@hotmail.com>
2019-03-30 10:59:01 -0400

    Commit con Modelo MVC

---
+++ Parcial/MVCParcial/Models/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCParcial.Models
+{
+    public enum TipoEstado
+
+    {
+        Ativo,
+        Inactivo
+    }
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+
+        public string Nombre { get; set; }
+        [Required]
+        
+        public decimal Precio { get; set; }
+
+        public int Cantidad { get; set; }
+        [Required]
+
+        public TipoUnidad Unidad { get; set; }
+
+        [Required]
+
+        public TipoEstado Estado { get; set; }
+
+        
+
+
+    }
+       
+}
